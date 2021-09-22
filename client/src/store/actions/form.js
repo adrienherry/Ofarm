91ec66493b3af8955d9144dc52d28744c1f6abcd
Mickaël Beljio <mbeljio@gmail.com>
2021-09-22 14:31:56 +0200

    S1 - registerForm - pages/homepage/carrousel et form

---
+++ client/src/store/actions/form.js
@@ -0,0 +1,41 @@
+export const SET_USERNAME = "SET_USERNAME";
+
+export const setUsername = (value) => ({
+  type: SET_USERNAME,
+  value,
+});
+
+export const SET_EMAIL = "SET_EMAIL";
+
+export const setEmail = (value) => ({
+  type: SET_EMAIL,
+  value,
+});
+
+export const SET_CONFIRM_EMAIL = "SET_CONFIRM_EMAIL";
+
+export const setConfirmEmail = (value) => ({
+  type: SET_CONFIRM_EMAIL,
+  value,
+});
+
+export const SET_PASSWORD = "SET_PASSWORD";
+
+export const setPassword = (value) => ({
+  type: SET_PASSWORD,
+  value,
+});
+
+export const SET_CONFIRM_PASSWORD = "SET_CONFIRM_PASSWORD";
+
+export const setConfigPassword = (value) => ({
+  type: SET_CONFIRM_PASSWORD,
+  value,
+});
+
+export const ADD_FORM = "ADD_FORM";
+
+export const addForm = (value) => ({
+  type: ADD_FORM,
+  value,
+});
--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,11 +1,12 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
+import formReducer from "./form";
 
 const rootReducer = combineReducers({
   // ici, on indique que notre state aurra une tranche (slice) appelée
   // recipes et que c'est le reducers recipesReducer (défini dans le module ./recipes)
   // qui en aura la charge
   // on accède à cette tranche du state par state.recipes
-
+  form: formReducer,
 });
 
 export default rootReducer;
