ebb67f5d68a132706e5ea0dca712393aa9baf150
Florian <florian.nguyen@yahoo.fr>
2021-09-23 20:44:45 +0200

    s1 - register - Added login and register routes

--- server/app/router.js
+++ server/app/router.js
@@ -1,19 +1,27 @@
 const { Router } = require("express");
 const router = Router();
+
 const speciesController = require("./controllers/speciesController");
 const userController = require("./controllers/userController");
 const gardenController = require("./controllers/gardenController");
 
-// const { homePage } = require("./controllers/mainController");
+const jwt = require("./services/jwtService");
 const authController = require("./controllers/authController");
-// router.get("/", homePage);
 
-// router.get("/", homePage); 
 router.get("/species", speciesController.findAll);
 router.get("/species/:id", speciesController.findOne);
+
+// USER ROUTES
 router.get("/user/:id", userController.findOne);
 router.get("/user/:user_id/garden/:garden_id", gardenController.findOneWithUserId);
+router.post(
+	"/user/:user_id/garden/:garden_id/species",
+	speciesController.addOneToGarden,
+);
 
-router.post("/user/:user_id/garden/:garden_id/species", speciesController.addOneToGarden)
+// AUTH ROUTES
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+// router.get("/logout", authController.logout);
 
 module.exports = router;
---
+++ server/app/services/jwtBlacklist.js
@@ -0,0 +1,14 @@
+const redisClient = require("./redisClient");
+const promisify = require("util");
+
+const asyncClient = {
+	get: promisify(redisClient.get).bind(redisClient),
+	set: promisify(redisClient.set).bind(redisClient),
+	setex: promisify(redisClient.setex).bind(redisClient),
+	del: promisify(redisClient.del).bind(redisClient),
+	exists: promisify(redisClient.exists).bind(redisClient),
+};
+
+const PREFIX = 'jwt-blacklist';
+const TIMEOUT = 60 * 60; // 1 hour
+const keys = [];
