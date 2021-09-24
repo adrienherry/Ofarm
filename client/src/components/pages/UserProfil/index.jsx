be42cc7642df2380885a50ea880d616dbee51803
Mickaël Beljio <mbeljio@gmail.com>
2021-09-24 12:28:39 +0200

    create component buttons,  rename User: UserProfil

--- client/src/components/pages/UserProfil/index.jsx
+++ client/src/components/pages/UserProfil/index.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import Buttons from './Buttons';
 import UserInformation from './UserInformation';
 import cow from '../../../../public/cow.jpg';
+import './user.scss';
 
-const User = () => (
-  <div>
+const UserProfil = () => (
+  <div className="UserProfil">
     <UserInformation img={cow} />
+    <Buttons />
   </div>
 );
 
-User.propTypes = {
+UserProfil.propTypes = {
 
 };
 
-export default User;
+export default UserProfil;
