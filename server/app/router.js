4b74f8eb20994f896735e2439f033434c89055f6
NicoLeo1030 <leonardonnicolas@gmail.com>
2021-09-24 10:10:58 +0200

    s1 - models - user informations modification route (PATCH) coded

--- server/app/router.js
+++ server/app/router.js
@@ -8,6 +8,13 @@ const gardenController = require("./controllers/gardenController");
 const jwt = require("./services/jwtService");
 const authController = require("./controllers/authController");
 
+
+// router.get("/", homePage); 
+
+// router.get("/user/:user_id/garden/:garden_id/species/remove/:species_id", userController.removeSpecies);
+// router.get("/user/:user_id/garden/remove/:garden_id", userController.removeGarden);
+
+
 router.get("/species", speciesController.findAll);
 router.get("/species/:id", speciesController.findOne);
 
@@ -23,5 +30,9 @@ router.post(
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 // router.get("/logout", authController.logout);
+
+router.post("/user/:user_id/garden/:garden_id/species", speciesController.addOneToGarden);
+
+router.patch("/user/:id", userController.save);
 
 module.exports = router;
