2b6d1d198819e68d946d6203baabdc9322bfd286
locchioclock@gmail.com <locchioclock@gmail.com>
2021-09-26 10:49:12 +0200

    404 Page - step 3 image CANVA

--- client/src/components/pages/NotFound/index.jsx
+++ client/src/components/pages/NotFound/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './not-found.scss';
-import chou from '/public/purchasing-2064952_640.jpg';
+// import chou from '/public/purchasing-2064952_640.jpg';
+
+import chou2 from '/public/chou404.jpg';
 
 const NotFound = () => (
 
   <div className="NotFound">
 
-    <img className="chou" src={chou} alt="chou blanc" />
+    <img className="chou2" src={chou2} alt="chou blanc" />
 
   </div>
 
