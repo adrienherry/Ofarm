71c8741327d9082debe16f8cbb5b5630d10380e8
Mickaël Beljio <mbeljio@gmail.com>
2021-09-29 16:16:04 +0200

    S2 - blockfield - component done

--- client/src/components/BlockField/index.jsx
+++ client/src/components/BlockField/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import { setUserDisabled } from '../../store/actions/profil';
 import './blockField.scss';
 
 const BlockField = ({
   value, type, name, placeholder, onChange,
 }) => {
+  const dispatch = useDispatch();
+  const disabled = useSelector((state) => state.profil.disabled);
+ 
   const handleChange = (event) => {
     onChange(event.target.value, name);
+  };
+
+  const handleClick = () => {
+    dispatch(setUserDisabled());
   };
 
   const inputId = `field-${name}`;
@@ -26,9 +35,15 @@ const BlockField = ({
           placeholder={placeholder}
           value={value}
           onChange={handleChange}
+          disabled={disabled}
         />
       </label>
-      <button type="button" className="blockField__button"> Modifier </button>
+      <button
+        type="button"
+        className="blockField__button"
+        onClick={handleClick}
+      > Modifier
+      </button>
     </div>
   );
 };
--- client/src/components/pages/UserProfil/UserProfilForm/index.jsx
+++ client/src/components/pages/UserProfil/UserProfilForm/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Grid } from '@material-ui/core';
-import { addProfil, setProfilField } from '../../../../store/actions/profil';
+import { addProfil, setProfilField, setUserEnabled } from '../../../../store/actions/profil';
 import Field from '../../../Field';
 import BlockField from '../../../BlockField';
 import './userProfilForm.scss';
@@ -11,6 +11,7 @@ const UserProfilForm = ({
   button, usernameProfil, emailProfil,
 }) => {
   const dispatch = useDispatch();
+  const inputRef = useRef();
 
   const handleChangeField = (value, name) => {
     dispatch(setProfilField(value, name));
@@ -20,6 +21,20 @@ const UserProfilForm = ({
     event.preventDefault();
     dispatch(addProfil());
   };
+
+  useEffect(() => {
+    const maybeHandler = (event) => {
+      if (inputRef.current && !inputRef.current.contains(event.target)) {
+        dispatch(setUserEnabled());
+      }
+    };
+
+    document.addEventListener('mousedown', maybeHandler);
+
+    return () => {
+      document.removeEventListener('mousedown', maybeHandler);
+    };
+  });
 
   return (
     <form onSubmit={handleSubmit} className="form">
@@ -31,6 +46,7 @@ const UserProfilForm = ({
             placeholder="Nom d'utilisateur"
             value={usernameProfil}
             onChange={handleChangeField}
+            ref={inputRef}
           />
         </Grid>
         <Grid item>
